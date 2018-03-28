364c8c0c13751765d13967521fe1df744747d978
firebotQL <viadukm@gmail.com>
2018-03-28 23:53:52 +0100

    Renamed example hello to skybet package

---
+++ back-end/src/main/java/skybet/FormRestController.java
@@ -0,0 +1,8 @@
+package skybet;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FormRestController {
+
+}
--- back-end/src/main/java/skybet/Greeting.java
+++ back-end/src/main/java/skybet/Greeting.java
@@ -1,4 +1,4 @@
-package hello;
+package skybet;
 
 public class Greeting {
 
