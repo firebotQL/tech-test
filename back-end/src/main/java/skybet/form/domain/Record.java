9e1906add94196e6304e4d21d473b1efcdf61a5b
firebotQL <viadukm@gmail.com>
2018-03-28 23:56:15 +0100

    Created Record domain object and added lombok library + annotations for clean code

---
+++ back-end/src/main/java/skybet/form/domain/Record.java
@@ -0,0 +1,14 @@
+package skybet.form.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Record {
+    private String id;
+    private String firstName;
+    private String sureName;
+}
