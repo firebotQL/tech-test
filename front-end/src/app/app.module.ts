192565f4625419f0a5a972b199f6b91bfcf739ea
firebotQL <viadukm@gmail.com>
2018-03-29 00:39:21 +0100

    Generated record form component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RecordFormComponent } from './record-form/record-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordFormComponent
   ],
   imports: [
     BrowserModule
