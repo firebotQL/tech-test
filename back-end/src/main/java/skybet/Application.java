bd328509c357013d73632a72a095acd6fcf3a456
firebotQL <viadukm@gmail.com>
2018-03-29 00:50:00 +0100

    Added CORS support from any origin for this tech test

--- back-end/src/main/java/skybet/Application.java
+++ back-end/src/main/java/skybet/Application.java
@@ -2,9 +2,24 @@ package skybet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
+
+    // Enable CORS requests from any origin
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
