499c6c29cd6fbe4574007c4a05917eea6b818ecb
firebotQL <viadukm@gmail.com>
2018-03-29 00:52:49 +0100

    Implemented fetching data of our record API and representing it in our form viewgit add .! Yahhooo :)

--- front-end/src/app/record-form/record-form.component.ts
+++ front-end/src/app/record-form/record-form.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { RecordService } from '../shared/record.service';
 
 @Component({
   selector: 'app-record-form',
   templateUrl: './record-form.component.html',
-  styleUrls: ['./record-form.component.css']
+  styleUrls: ['./record-form.component.css'],
+  providers: [RecordService]
 })
 export class RecordFormComponent implements OnInit {
 
-  constructor() { }
+  records = null;
+
+  constructor(
+    private recordService: RecordService
+  ) {
+    this.loadRecords();
+  }
 
   ngOnInit() {
   }
 
+  loadRecords() {
+    this.recordService.getAll().subscribe(data => this.records = data);
+  }
 }
--- front-end/src/app/shared/record.service.ts
+++ front-end/src/app/shared/record.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Record } from '../record';
 
 @Injectable()
 export class RecordService {
+  private apiUrl = 'http://localhost:8080/record';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll() {
+    return this.http.get<Record>(this.apiUrl)
+      .map(response => response);
+  }
 
 }
